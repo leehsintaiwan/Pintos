19224cebc4484058c0b34ae10e3942ed7fcb1497
Joel Chu <jyc20@texel05.doc.ic.ac.uk>
2021-10-17 22:47:02 +0100

    fixed point arithmetic operations

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -10,9 +10,27 @@
 #define integer_to_fixed_point (n) (n * f)
 #define fixed_point_to_integer_zero (x) (x / f)
 
-inline uint32_t fixed_point_to_integer_nearest (uint32_t x)
+inline int32_t fixed_point_to_integer_nearest (int32_t x)
 {
-    x >= 0 ? return (x + f / 2) / f : return (x - f / 2) / f;
+    return x >= 0 ? (x + f / 2) / f : (x - f / 2) / f;
+}
+
+/* Binary operations */
+#define add_fixed_points(x, y) (x + y)
+#define subtract_fixed_points(x, y) (x - y)
+#define add_fixed_and_integer(x, n) (x + n * f)
+#define subtract_integer_from_fixed(x, n) (x - n * f)
+#define multiply_fixed_and_integer(x, n) (x * n)
+#define divide_fixed_by_integer(x, n) (x / n)
+
+inline int64_t multiply_fixed_points (int32_t x, int32_t y)
+{
+    return ((int64_t) x) * y / f;
+}
+
+inline int64_t divide_fixed_points ()
+{
+    return ((int64_t) x) * f / y;
 }
 
 #endif /* threads/fixed-point.h */
