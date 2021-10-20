17738bd78609bcec9f225159a550e368bf7f8ae0
Vincent Lee <vl620@ic.ac.uk>
2021-10-20 14:30:19 +0100

    Added intrrupt disable in lock acquire

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -224,13 +224,13 @@ lock_acquire (struct lock *lock)
   ASSERT (!intr_context ());
   ASSERT (!lock_held_by_current_thread (lock));
 
-  //old_level = intr_disable();
+  enum intr_level old_level = intr_disable();
   struct thread *current = thread_current();
   if (lock->holder)
   {
     list_push_back(&lock->holder->donators, &current->donator_elem);
   }
-
+  intr_set_level(old_level);
   sema_down (&lock->semaphore);
   lock->holder = thread_current ();
 }
