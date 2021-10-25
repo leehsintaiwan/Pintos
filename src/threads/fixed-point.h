8b33c22b6a400cbbb2210d762c0423bb45f40b85
Joel Chu <jyc20@texel05.doc.ic.ac.uk>
2021-10-25 15:15:07 +0100

    changed inline functions to macros in fixed-point

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -11,11 +11,7 @@
 /* Conversion macros */
 #define INTEGER_TO_FIXED_POINT (n) (n * F)
 #define FIXED_POINT_TO_INTEGER_ZERO (x) (x / F)
-
-inline int32_t FIXED_POINT_TO_INTEGER_NEAREST (int32_t x)
-{
-    return x >= 0 ? (x + F / 2) / F : (x - F / 2) / F;
-}
+#define FIXED_POINT_TO_INTEGER_NEAREST (x) ((x >= 0) ? ((x + F / 2) / F) : ((x - F / 2) / F))
 
 /* Binary operations */
 #define ADD_FIXED_POINTS(x, y) (x + y)
@@ -24,15 +20,7 @@ inline int32_t FIXED_POINT_TO_INTEGER_NEAREST (int32_t x)
 #define SUBTRACT_INTEGER_FROM_FIXED(x, n) (x - n * F)
 #define MULTIPLY_FIXED_AND_INTEGER(x, n) (x * n)
 #define DIVIDE_FIXED_BY_INTEGER(x, n) (x / n)
-
-inline int64_t MULTIPLY_FIXED_POINTS (int32_t x, int32_t y)
-{
-    return ((int64_t) x) * y / F;
-}
-
-inline int64_t DIVIDE_FIXED_POINTS (int32_t x, int32_t y)
-{
-    return ((int64_t) x) * F / y;
-}
+#define MULTIPLY_FIXED_POINTS (x, y) (x *y / F)
+#define DIVIDE_FIXED_POINTS (x, y) (x * F / y)
 
 #endif /* threads/fixed-point.h */
