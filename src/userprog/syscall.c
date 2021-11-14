5033a8c1f6c6a6b31275bbcdc25e4e05e190f73f
Sipan Petrosyan <sdp120@ic.ac.uk>
2021-11-14 01:38:03 +0000

    Added filesystem lock Added user address validation functions

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -141,3 +141,38 @@ static unsigned tell (int fd)
 // Closes fd
 static void close (int fd)
 {}
+
+
+/* Reads a byte at user virtual address UADDR.
+UADDR must be below PHYS_BASE.
+Returns the byte value if successful, -1 if a segfault
+occurred. */
+static int
+get_user (const uint8_t *uaddr)
+{
+  /* Check user address is below PHYS_BASE. */
+  if (!is_user_vaddr (uaddr))
+  {
+    return -1;
+  }
+
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:" : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+/* Writes BYTE to user address UDST.
+UDST must be below PHYS_BASE.
+Returns true if successful, false if a segfault occurred. */
+static bool
+put_user (uint8_t *udst, uint8_t byte)
+{
+  /* Check user address is below PHYS_BASE. */
+  if (!is_user_vaddr (udst))
+  {
+    return false;
+  }
+
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:" : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+  return error_code != -1;
+}
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -3,4 +3,7 @@
 
 void syscall_init (void);
 
+/* Lock to be used when accessing the file system. */
+struct lock filesys_lock;
+
 #endif /* userprog/syscall.h */
