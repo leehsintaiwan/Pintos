30dde56342a50e24486eee0fdba14b6822e7df8c
Jamie Todd <jmt20@ic.ac.uk>
2021-11-27 20:33:56 +0000

    adapted frame functions for pages

--- src/vm/page.c
+++ src/vm/page.c
@@ -1,1 +1,21 @@
-#include "vm/page.h"
+#include "page.h"
+
+static struct supp_page_table *supp_page_table;
+
+void init_pages()
+{
+  hash_init(&supp_page_table->page_table, hash_func, hash_less, NULL);
+}
+
+uint32_t hash_func(const struct hash_elem *e, void *aux UNUSED)
+{
+  struct page *page = hash_entry(e, struct page, elem);
+  return page->address;
+}
+
+bool hash_less(const struct hash_elem *a, const struct hash_elem *b, void *aux)
+{
+  struct page *page_a = hash_entry(a, struct page, elem);
+  struct page *page_b = hash_entry(b, struct page, elem);
+  return page_a->address < page_b->address;
+}
--- src/vm/page.h
+++ src/vm/page.h
@@ -1,4 +1,14 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+struct supp_page_table {
+    struct hash page_table;
+    // supplementary features(?)
+};
+
 struct page {
     uint32_t address;
     bool read_only;
-}
+    struct hash_elem elem;
+};
