ef0c1d8e64de72b63add1b134c0a4a1202b2179e
Sipan Petrosyan <sdp120@ic.ac.uk>
2021-11-29 00:23:32 +0000

    Changed method signature for helpers in page.c Added destroy function and add frame to supp_pt functions

--- src/vm/page.c
+++ src/vm/page.c
@@ -1,21 +1,78 @@
 #include "page.h"
+#include "threads/malloc.h"
+#include "userprog/syscall.h"
 
-static struct supp_page_table *supp_page_table;
 
-void init_pages()
+static uint32_t supp_hash_func(const struct hash_elem *e, void *aux UNUSED);
+static bool supp_hash_less(const struct hash_elem *a, const struct hash_elem *b, void *aux UNUSED);
+static void supp_destroy_func (struct hash_elem *e, void *aux UNUSED);
+
+/* Create supplemental page table */
+struct supp_page_table *init_supp_page_table (void)
 {
-  hash_init(&supp_page_table->page_table, hash_func, hash_less, NULL);
+  struct supp_page_table *supp_page_table = 
+    (struct supp_page_table*) malloc (sizeof (struct supp_page_table));
+
+  // If there is no memory to allocate then fail.
+  if (!supp_page_table)
+  {
+    exit_exception ();
+  }
+  
+  // Initialise the supplemtnale page table.
+  hash_init (&supp_page_table->page_table, supp_hash_func, supp_hash_less, NULL);
+  
+  return supp_page_table;
 }
 
-uint32_t hash_func(const struct hash_elem *e, void *aux UNUSED)
+/* Destroy supplemental page table. */
+void destroy_supp_pt (struct supp_page_table *supp_page_table)
 {
-  struct page *page = hash_entry(e, struct page, elem);
-  return page->address;
+  hash_destroy (&supp_page_table->page_table, supp_destroy_func);
+  free (supp_page_table);
 }
 
-bool hash_less(const struct hash_elem *a, const struct hash_elem *b, void *aux)
+
+/* Add page with location FRAME to supplemental page table. */
+bool add_frame_supp_pt (struct supp_page_table *supp_page_table, void *addr)
 {
-  struct page *page_a = hash_entry(a, struct page, elem);
-  struct page *page_b = hash_entry(b, struct page, elem);
+  struct page *page = (struct page *) malloc(sizeof(struct page));
+
+  // Set page attributes.
+  page->address = addr;
+  page->page_from = FRAME;
+  page->dirty_bit = false;
+
+  struct hash_elem *prev_elem = hash_insert (&supp_page_table->page_table, &page->elem);
+  // Return true if successful and false if the page already exists (unsuccessful).
+  if (!prev_elem) {
+    return true;
+  }
+  else 
+  {
+    free (page);
+    return false;
+  }
+}
+
+
+/* Helper functions for the supplemental page table hash map. */
+
+static uint32_t supp_hash_func(const struct hash_elem *e, void *aux UNUSED)
+{
+  struct page *page = hash_entry (e, struct page, elem);
+  return hash_int((int) page->address);
+}
+
+static bool supp_hash_less(const struct hash_elem *a, const struct hash_elem *b, void *aux UNUSED)
+{
+  struct page *page_a = hash_entry (a, struct page, elem);
+  struct page *page_b = hash_entry (b, struct page, elem);
   return page_a->address < page_b->address;
+}
+
+static void supp_destroy_func (struct hash_elem *e, void *aux UNUSED)
+{
+  struct supp_pt_entry *entry = hash_entry (e, struct page, elem);
+  free (entry);
 }
--- src/vm/page.h
+++ src/vm/page.h
@@ -1,14 +1,47 @@
+#ifndef VM_PAGE_H
+#define VM_PAGE_H
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <hash.h>
+
+
+
+/* The different locations/states
+   the page can be in. */
+enum page_loc {
+  FRAME, /* In memory. */
+  ZERO, /* Zeros. */
+  SWAP, /* In swap slot. */
+  FILE /* In filesys or executable. */
+};
+
 
 struct supp_page_table {
+    /* Map from page to the supplemental page table entry. */
     struct hash page_table;
-    // supplementary features(?)
 };
 
+/* A page is an entry of the supp_page_table. */
 struct page {
-    uint32_t address;
+    void *address; /* Virtual address of the page. */
+
+    enum page_loc page_from;
+
+    bool dirty_bit;
+
+
+
     bool read_only;
-    struct hash_elem elem;
+    struct hash_elem elem; 
 };
+
+
+struct supp_page_table *init_supp_page_table (void);
+void destroy_supp_pt (struct supp_page_table *supp_page_table);
+bool add_frame_supp_pt (struct supp_page_table *supp_page_table, void *addr);
+
+
+
+#endif
