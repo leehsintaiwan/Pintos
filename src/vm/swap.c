c27dcd560fe10845245a2debdf5d19dfe9229f5b
Jamie Todd <jmt20@ic.ac.uk>
2021-12-04 16:57:18 +0000

    swap_read implemented

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,25 +1,45 @@
 #include "vm/swap.h"
 #include "devices/block.h"
+#include "threads/vaddr.h"
 #include <bitmap.h>
 
 #define SECTORS_PER_PAGE (PGSIZE / BLOCK_SECTOR_SIZE)
 
 static struct block *block_swap;
-static struct bitmap *swap_bitmap; // bitmap for checking availiable swap slots
+static struct bitmap *swap_bitmap; // Bitmap for checking availiable swap slots
+static size_t swap_table_size; // The size of the swap table (number of swap slots)
 
 // Initialise the swap table
 void swap_init (void) 
 {
     block_swap = block_get_role(BLOCK_SWAP);
-    size_t swap_size = block_size(block_swap) / SECTORS_PER_PAGE;
-    swap_bitmap = bitmap_create(swap_size);
+    swap_table_size = block_size(block_swap) / SECTORS_PER_PAGE;
+    swap_bitmap = bitmap_create(swap_table_size);
     bitmap_set_all(swap_bitmap, true);
 }
 
 // Read the content from the swap index, and store into "page"
 void swap_read (uint32_t swap_index, void *page) 
 {
+  // Assert that the page and swap index are both valid  
+  ASSERT (page >= PHYS_BASE);
+  ASSERT (swap_index < swap_table_size);
 
+  // Assert that the slot is not empty  
+  if (bitmap_test(swap_bitmap, swap_index)) 
+  {
+    printf("ERROR: Attempted to read from empty swap slot");
+    return;
+  }
+
+  // Read the slot at the index and store in page  
+  for (int i = 0; i < SECTORS_PER_PAGE; i++) 
+  {
+    block_read (block_swap, swap_index * SECTORS_PER_PAGE + i, page + BLOCK_SECTOR_SIZE * i);
+  }
+
+  // Make the index availiable to write again  
+  bitmap_set(swap_bitmap, swap_index, true);
 }
 
 // Write the content of "page" into swap table
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -9,11 +9,13 @@ void swap_read (uint32_t swap_index, void *page);
 uint32_t swap_write (void *page);
 void free_swap (uint32_t swap_index);
 
-static struct swap_table {
+static struct swap_table 
+{
     // TO-DO
 };
 
-struct swap_slot {
+struct swap_slot 
+{
     // TO-DO
 };
 
