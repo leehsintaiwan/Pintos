f6a6c3c63e582e469529ffa1d0fec7dc6b2f20a2
Jamie Todd <jmt20@ic.ac.uk>
2021-12-09 09:33:51 +0000

    made style consistent in page.h

--- src/vm/page.h
+++ src/vm/page.h
@@ -8,49 +8,47 @@
 #include <stdint.h>
 #include <hash.h>
 
-
-
 /* The different locations/states
    the page can be in. */
-enum page_loc {
+enum page_loc 
+{
   FRAME, /* In memory. */
   ZERO, /* Zeros. */
   SWAP, /* In swap slot. */
   EXECFILE /* In filesys/executable. */
 };
 
-
-struct supp_page_table {
-    /* Map from page to the supplemental page table entry. */
-    struct hash page_table;
+struct supp_page_table 
+{
+  /* Map from page to the supplemental page table entry. */
+  struct hash page_table;
 };
 
 /* A page is an entry of the supp_page_table. */
-struct page {
-    void *address; /* Virtual address of the page. */
-    void *faddress; /* Frame address of the page (location in the kernel). */
-    enum page_loc page_from;
+struct page 
+{
+  void *address; /* Virtual address of the page. */
+  void *faddress; /* Frame address of the page (location in the kernel). */
+  enum page_loc page_from;
+  bool dirty_bit;
 
+  // When page loc is SWAP
+  uint32_t swap_index;
 
-    bool dirty_bit;
+  // When page loc is FILE
+  struct file_struct *file_info;
 
-    // When page loc is SWAP
-    uint32_t swap_index;
-
-    // When page loc is FILE
-    struct file_struct *file_info;
-
-    struct hash_elem elem; 
+  struct hash_elem elem; 
 };
 
-struct file_struct {
+struct file_struct 
+{
   struct file *file; /* Executable file. */
   int32_t file_start_byte; /*  Offset in the file to start read. */
   size_t file_read_bytes; /* Number of bytes to read. */
   size_t file_zero_bytes; /* Number of trailing 0 bytes. */
   bool file_writeable;  /* Is file writeable (based on segment being read). */
 };
-
 
 struct supp_page_table *init_supp_page_table (void);
 void destroy_supp_pt (struct supp_page_table *supp_page_table);
@@ -64,8 +62,5 @@ bool load_page (struct supp_page_table *supp_page_table, uint32_t *pagedir, void
 bool add_supp_pt (struct supp_page_table *supp_page_table, void *addr, void *faddr, enum page_loc from, struct file_struct *file_info);
 bool unmap_supp_pt(struct supp_page_table *supp_page_table, uint32_t *pagedir,
     void *addr, struct file *f, uint32_t offset, size_t bytes);
-
-
-
 
 #endif
